f42dc2b3878607db32a1c15a42436308e4951d10
kozian <kozian@gmail.com>
2012-05-18 11:15:28 +0400

    my first commit to github

---
+++ sampleSEproject/src/GeneralPackage/HelloWorld.java
@@ -0,0 +1,15 @@
+package GeneralPackage;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		for (int i=5;i>2;i--) sayHelloWorld();
+	}
+	
+	private static void sayHelloWorld() {
+		System.out.println("Hello my Sample WORLD!");
+	}
+}
